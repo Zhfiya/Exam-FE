2ae6caa81cd7baad4a987ea68450359e4a96cc30
yuyan <Paranoid_ZH@163.com>
2021-03-24 14:03:28 +0800

    back

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,16 @@
       <el-header class="header">
         <the-header></the-header>
       </el-header>
-      <el-container class="main">
-        <el-aside>
-          <the-aside></the-aside>
-        </el-aside>
-        <el-container>
-          <el-main>
-            <router-view />
-          </el-main>
-        </el-container>
-      </el-container>
     </el-container>
+    <router-view />
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader";
-import TheAside from "./components/TheAside";
 export default {
   components: {
     TheHeader,
-    TheAside,
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -7,23 +7,11 @@ import {
   Header,
   Container,
   Tooltip,
-  Menu,
-  MenuItemGroup,
-  MenuItem,
-  Submenu,
-  Aside,
-  Main,
 } from "element-ui";
 
 Vue.use(Header);
 Vue.use(Container);
 Vue.use(Tooltip);
-Vue.use(Menu);
-Vue.use(MenuItemGroup);
-Vue.use(MenuItem);
-Vue.use(Submenu);
-Vue.use(Aside);
-Vue.use(Main);
 
 Vue.config.productionTip = false;
 
